497816c5a523155c3e456a11d7db6ea94fa0d056
Salvatore Amplo <salvatoreamplo@gmail.com>
2026-03-23 11:45:32 +0100

    fix login

--- xalvautoBE/src/main/java/salvoamplo/xalvautobe/controllers/AuthController.java
+++ xalvautoBE/src/main/java/salvoamplo/xalvautobe/controllers/AuthController.java
@@ -2,7 +2,10 @@ package salvoamplo.xalvautobe.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import salvoamplo.xalvautobe.payloads.AdminLoginRequest;
 import salvoamplo.xalvautobe.payloads.AdminLoginResponse;
@@ -10,7 +13,6 @@ import salvoamplo.xalvautobe.services.JwtService;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final JwtService jwtService;
