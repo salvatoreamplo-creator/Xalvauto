c1ca5245d11d580fade312ea9f62f842b6bc8733
Salvatore Amplo <salvatoreamplo@gmail.com>
2026-03-23 20:39:52 +0100

    test definitivo login

--- xalvautoBE/src/main/java/salvoamplo/xalvautobe/controllers/AuthController.java
+++ xalvautoBE/src/main/java/salvoamplo/xalvautobe/controllers/AuthController.java
@@ -22,18 +22,6 @@ public class AuthController {
 
     @PostMapping("/login")
     public AdminLoginResponse login(@RequestBody AdminLoginRequest request) {
-
-        String email = request.getEmail() == null ? "" : request.getEmail().trim();
-        String password = request.getPassword() == null ? "" : request.getPassword().trim();
-
-        System.out.println("EMAIL RICEVUTA = [" + email + "]");
-        System.out.println("PASSWORD RICEVUTA = [" + password + "]");
-
-        if (!"salvatoreamplo@gmail.com".equalsIgnoreCase(email) || !"salvo434".equals(password)) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Credenziali non valide");
-        }
-
-        String token = jwtService.generateToken(email);
-        return new AdminLoginResponse(token);
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "TEST123456789");
     }
 }
